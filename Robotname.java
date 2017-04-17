5ea049dfbda7038bc87787e4c90837bac3282cab
kiannamcollins <Kianna.collins@gompersstudent.org>
2017-04-17 14:56:02 -0700

    Add files via upload

---
+++ Robotname.java
@@ -0,0 +1,21 @@
+package robot;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class Robotname {
+public static void main(String[] args) {
+Robot K  = new Robot();	
+K.penDown();
+K.move(200);
+K.turn(180);
+K.move(100);
+K.turn(-145);
+K.move(100);
+K.turn(-145);
+K.move(100);
+	
+	
+	
+	
+}
+}
